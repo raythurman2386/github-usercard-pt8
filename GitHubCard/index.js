ccebb43a7244d612164f8209595180b2b5b2ca93
Ray Thurman <raymondthurman5@gmail.com>
2019-08-15 10:59:46 -0400

    updated followers array to be all our section

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -36,7 +36,16 @@ axios
           user, and adding that card to the DOM.
 */
 
-const followersArray = [ 'miklo88', 'nickdurbin', 'timothyshores', 'tetondan', 'dustinmyers' ];
+const sectionArray = [
+	'miklo88',
+	'nickdurbin',
+	'Fractured2k',
+	'leachcoding',
+	'bobbidigi',
+	'AislynnEdmiston',
+	'IanCarreras',
+	'Sherexmykes',
+];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
