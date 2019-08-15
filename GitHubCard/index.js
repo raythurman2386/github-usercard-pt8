0e1ad6a45fa5094a212d9bb1180c92f52fe48805
Ray Thurman <raymondthurman5@gmail.com>
2019-08-15 11:17:45 -0400

    mvp complete, needs refactor

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,17 +3,21 @@ const container = document.querySelector('.cards');
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-axios
-	.get(`https://api.github.com/users/raythurman2386`)
-	.then((res) => {
-		const data = res.data;
-		console.log(data);
-		const card = cardCreator(data);
-		container.appendChild(card);
-	})
-	.catch((error) => {
-		console.log(error);
-	});
+function getData(user) {
+	axios
+		.get(`https://api.github.com/users/${user}`)
+		.then((res) => {
+			const data = res.data;
+			// console.log(data);
+			const card = cardCreator(data);
+			container.appendChild(card);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
+
+getData('raythurman2386');
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -46,6 +50,13 @@ const sectionArray = [
 	'IanCarreras',
 	'Sherexmykes',
 ];
+
+setTimeout(
+	sectionArray.forEach((user) => {
+		getData(user);
+	}),
+	1000,
+);
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
